d555740d7cad266f82a2480b01bbd28658ee9f18
ryan <ryanchen900218@gmail.com>
2025-08-30 18:02:11 +0800

    init: minimal app with Dockerfile

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    return "Hello from Flask on Kubernetes (port 3000)!"
+
+if __name__ == "__main__":
+    # 在容器內聽 0.0.0.0:3000
+    app.run(host="0.0.0.0", port=3000)
